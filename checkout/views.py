8a2e91bffdeaf9fb4f83540ee24e9e0bd6c0043a
sunshine274 <jingx274@gmail.com>
2020-12-16 20:36:58 +0000

    added basic stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
-
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
@@ -9,6 +8,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing here yet")
@@ -17,13 +19,18 @@ def checkout(request):
     current_bag = bag_contents(request)
     current_total = current_bag['grand_total']
     stripe_total = round(current_total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HwG20ASaZPrwd1o54Pvx9kO9bpAJs6Sp0uDqOtva6rBa6ZvC4dIETNdeEyd80yuiuuxSqweVKTUUAHhqwIt8IQV00HO5c61Ev',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
