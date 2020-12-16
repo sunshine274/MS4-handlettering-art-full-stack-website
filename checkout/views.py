c17cdc4158d3fce88c0d995e727ac1e4a23704c3
sunshine274 <jingx274@gmail.com>
2020-12-16 10:21:56 +0000

    added forms, admin and signals for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request. "There's nothing here yet")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
