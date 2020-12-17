aae565f6212423ce3387453dc8c1ecace0f10c51
sunshine274 <jingx274@gmail.com>
2020-12-17 16:43:02 +0000

    updated checkout form

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,11 +12,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                         'order_total',
-                       'grand_total', )
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
-               'country',
-              'order_total', 'grand_total',)
+                'country', 'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'original_bag',
+              'stripe_pid', 'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total',
