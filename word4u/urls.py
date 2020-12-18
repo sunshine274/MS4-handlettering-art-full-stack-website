781e9e909c74533f4e90ab1949e57fad3b2eefab
sunshine274 <jingx274@gmail.com>
2020-12-18 10:39:54 +0000

    added whitenoise

--- word4u/urls.py
+++ word4u/urls.py
@@ -27,4 +27,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'word4u.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='{}/static/'.format(settings.BASE_DIR), prefix='static/')
+application.add_files('{}/media/'.format(settings.BASE_DIR), prefix='media/')
