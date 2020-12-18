31688a5c1aa14981bb264e82317631d1114b67dd
sunshine274 <jingx274@gmail.com>
2020-12-18 21:53:45 +0000

    modified settings

--- word4u/settings.py
+++ word4u/settings.py
@@ -121,22 +121,22 @@ WSGI_APPLICATION = 'word4u.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#      'default': dj_database_url.parse('postgres://dyslvbwnffagoz:703b6adf11ae36564b9c591841051378c56cbabff2167e02d406f4b62809ab48@ec2-52-212-157-46.eu-west-1.compute.amazonaws.com:5432/d391hjkqct72a')
-#  }
+DATABASES = {
+     'default': dj_database_url.parse('postgres://dyslvbwnffagoz:703b6adf11ae36564b9c591841051378c56cbabff2167e02d406f4b62809ab48@ec2-52-212-157-46.eu-west-1.compute.amazonaws.com:5432/d391hjkqct72a')
+ }
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation
