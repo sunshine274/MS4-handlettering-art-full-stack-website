f2785231354cb192b6c4bbfdf61b7b43a065d564
sunshine274 <jingx274@gmail.com>
2020-12-18 15:54:35 +0000

    modified settings.py to deploy static and media files

--- word4u/settings.py
+++ word4u/settings.py
@@ -17,10 +17,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'ms4-lettering-design-e-shop'
-    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+
 
 
 
@@ -186,6 +183,21 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'ms4-lettering-design-e-shop'
+    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOME_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOME_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOME_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 BUNDLE_DISCOUNT_THRESHOLD = 49.99
