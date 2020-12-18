20e38f8e8e602131d0691f4fc7f82be1a4b2e853
sunshine274 <jingx274@gmail.com>
2020-12-18 17:21:36 +0000

    modified secret key

--- word4u/settings.py
+++ word4u/settings.py
@@ -22,10 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = ''
+
+# os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-lettering-design-e-shop.herokuapp.com', 
                 'localhost']
